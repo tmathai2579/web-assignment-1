e1445df02fe4d9d90ca080924e18a97feaf2959a
tmathai2579 <tmathai2579@conestogac.on.ca>
2024-06-13 11:23:55 -0400

    Add testimonial page with dynamic data rendering from data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const testimonials = [
+    {
+        rating: 5,
+        heading: "Exceptional Haircut and Threading Experience with Friendly Staff",
+        body: "I had an amazing experience for hair cutting and threading. I do know the name of the hair dresser, but she has done awesome job, and very detailing while doing threading and haircut. Amazing and very friendly staff. I will definitely come again",
+        reviewer: "Yesha Patel",
+        source: "Google Review"
+    },
+    {
+        rating: 5,
+        heading: "A Top-Notch Barbering Experience with Vimal at Fademan",
+        body: "I recently had the pleasure of getting my haircut at Fademan, and I must say, Vimal is a true artist in the world of barbering. From the moment I stepped into the shop, Vimal greeted me with a warm and friendly demeanor that set the tone for a fantastic experience.",
+        reviewer: "Anonymous",
+        source: "Google Review"
+    },
+    {
+        rating: 5,
+        heading: "Exemplary Service and Exceptional Styling at Fademan Hair Salon",
+        body: "Fademan Hair Salon in Kitchener stands out as a beacon of professionalism and excellence in the world of hairstyling. I recently had the pleasure of visiting this salon, and I must say, it exceeded all my expectations.",
+        reviewer: "Anonymous",
+        source: "Google Review"
+    }
+];
+
+const services = [
+    {}
+];
+
+export { testimonials, services };
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import { testimonials, services } from './data.js'
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+// const testimonials = [
+//     {
+//         rating: 5,
+//         heading: "Exceptional Haircut and Threading Experience with Friendly Staff",
+//         body: "I had an amazing experience for hair cutting and threading. I do know the name of the hair dresser, but she has done awesome job, and very detailing while doing threading and haircut. Amazing and very friendly staff. I will definitely come again",
+//         reviewer: "Yesha Patel",
+//         source: "Google Review"
+//     },
+//     {
+//         rating: 5,
+//         heading: "A Top-Notch Barbering Experience with Vimal at Fademan",
+//         body: "I recently had the pleasure of getting my haircut at Fademan, and I must say, Vimal is a true artist in the world of barbering. From the moment I stepped into the shop, Vimal greeted me with a warm and friendly demeanor that set the tone for a fantastic experience.",
+//         reviewer: "Anonymous",
+//         source: "Google Review"
+//     }
+// ];
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
@@ -17,6 +35,9 @@ app.get("/service", (req, res) => {
     res.render("service.ejs");
 })
 
+app.get("/testimonials", (req, res) => {
+    res.render("testimonials.ejs", { testimonials });
+})
 
 
 app.listen(port, () => {
