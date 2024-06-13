0cf0a0b54e0db9ab47c86923c967bc9b793676e7
tmathai2579 <tmathai2579@conestogac.on.ca>
2024-06-13 09:42:21 -0400

    Set up project structure with initial routes
    - Created a basic project structure for the Express application
    - Added a 'views' directory with 'partials' subdirectory for templates
    - Added a 'public' directory with 'css' and 'images' subdirectories for static assets

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    console.log("/ called");
+    res.send("Hello Tinu Mathai");
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
