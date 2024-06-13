1dbb25f687679a06fb467ada210f862a6bbcd138
tmathai2579 <tmathai2579@conestogac.on.ca>
2024-06-13 12:18:09 -0400

    update header, include 2 new css files
    - Style added on header
    - added 2 new css files utlities and variables

--- index.js
+++ index.js
@@ -6,37 +6,29 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-// const testimonials = [
-//     {
-//         rating: 5,
-//         heading: "Exceptional Haircut and Threading Experience with Friendly Staff",
-//         body: "I had an amazing experience for hair cutting and threading. I do know the name of the hair dresser, but she has done awesome job, and very detailing while doing threading and haircut. Amazing and very friendly staff. I will definitely come again",
-//         reviewer: "Yesha Patel",
-//         source: "Google Review"
-//     },
-//     {
-//         rating: 5,
-//         heading: "A Top-Notch Barbering Experience with Vimal at Fademan",
-//         body: "I recently had the pleasure of getting my haircut at Fademan, and I must say, Vimal is a true artist in the world of barbering. From the moment I stepped into the shop, Vimal greeted me with a warm and friendly demeanor that set the tone for a fantastic experience.",
-//         reviewer: "Anonymous",
-//         source: "Google Review"
-//     }
-// ];
-
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", { 
+        currentPath: '/'
+    });
 })
 
 app.get("/about-us", (req, res) => {
-    res.render("about-us.ejs");
+    res.render("about-us.ejs", { 
+        currentPath: '/about-us'
+    });
 })
 
 app.get("/service", (req, res) => {
-    res.render("service.ejs");
+    res.render("service.ejs", { 
+        currentPath: '/service'
+    });
 })
 
 app.get("/testimonials", (req, res) => {
-    res.render("testimonials.ejs", { testimonials });
+    res.render("testimonials.ejs", { 
+        currentPath: '/testimonials',
+        testimonials: testimonials 
+    });
 })
 
 
